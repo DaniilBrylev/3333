5b96bad1369b4487e830a0ba3d3748c9aeca5dc1
DaniilBrylev <dan.brylev38@gmail.ru>
2024-12-29 00:50:38 +0300

    1

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+    @Test
+    void simpleTest() {
+        assertEquals(2 + 2, 4);
+    }
+}
